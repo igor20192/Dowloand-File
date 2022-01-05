bfa1d29b764c70454b3543747d6f0050e15465ac
igor20192 <igor.udovenko2012@meta.ua>
2022-01-05 12:02:20 +0200

    Добавьте файлы проекта.

---
+++ dowfile.err.tcl
@@ -0,0 +1,18 @@
+can not find channel named "stderr"
+    while executing
+"puts stderr $result"
+    (procedure "vTcl:python_run" line 16)
+    invoked from within
+"vTcl:python_run $base $prefix"
+    (procedure "vTcl:python_quick_run" line 11)
+    invoked from within
+"vTcl:python_quick_run .vTcl.gui_console gui"
+    invoked from within
+".vTcl.gui_console.butframe.but34 invoke"
+    ("uplevel" body line 1)
+    invoked from within
+"uplevel #0 [list $w invoke]"
+    (procedure "tk::ButtonUp" line 24)
+    invoked from within
+"tk::ButtonUp .vTcl.gui_console.butframe.but34"
+    (command bound to event)
